d6b111a9cf915cd1f099886d85946fd5d12775fc
James <jrmidkiff@live.com>
2017-12-28 10:25:41 -0500

    This is the beginning

---
+++ build_site.R
@@ -0,0 +1,7 @@
+# Follow these directions STEP BY STEP http://nickstrayer.me/RMarkdown_Sites_tutorial/
+# Must always have an index.Rmd file
+# Must always save the index.Rmd and the other .Rmd files running the below commands
+
+setwd("D:/Everything/R/Politifact Investigation/PolitiFact-Analysis/PolitiFact-Analysis")
+
+rmarkdown::render_site()
